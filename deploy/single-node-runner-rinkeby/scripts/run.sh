4382cd346b690afba9fb9a3024980f2b7b3ac81c
dzmitryhil <dzmitryhil@gmail.com>
2021-09-24 14:16:28 +0300

    - restore dev branch for runner

--- deploy/single-node-runner-rinkeby/scripts/run.sh
+++ deploy/single-node-runner-rinkeby/scripts/run.sh
@@ -72,8 +72,8 @@ geth --rinkeby --syncmode "light" \
 GETH_IPC_PATH="/root/.ethereum/rinkeby/geth.ipc"
 GETH_CONSOLE="geth --rinkeby attach ipc:$GETH_IPC_PATH console --exec"
 
-# 600 sec to run light node
-for i in {1..600}; do
+# 600000 sec to run light node
+for i in {1..600000}; do
   sleep 1
   echo "attempt $i to start the eth node"
 
@@ -89,7 +89,7 @@ for i in {1..600}; do
      continue
   fi
 
-  if [ $i -eq 600 ]; then
+  if [ $i -eq 600000 ]; then
      echo "timeout for ethereum light node exceed"
      exit
   fi
