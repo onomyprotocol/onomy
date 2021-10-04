084796801ddc3c12fdc4fad0e0c24f978adb26b0
Parth Sanghani <parth@parth-onomy.io>
2021-10-04 18:25:47 -0500

    deploy->testnet->bin.sh moved installation of nodejs from middle of the file to top, with other packages

--- deploy/testnet/bin.sh
+++ deploy/testnet/bin.sh
@@ -1,21 +1,18 @@
 echo "-----------Installing_dependencies---------------"
-
 dnf -y update
 dnf -y copr enable ngompa/musl-libc
 dnf -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
 sudo subscription-manager repos --enable codeready-builder-for-rhel-8-x86_64-rpms
-dnf -y install curl nano ca-certficates tar git jq gcc-c++ musl-devel musl-gcc golang gmp-devel perl python3 moreutils wget
+dnf -y install curl nano ca-certficates tar git jq gcc-c++ musl-devel musl-gcc golang gmp-devel perl python3 moreutils wget nodejs
 
 HOME_DIRECTORY=$HOME
 
 echo "--------------installing_rust---------------------------"
-
 curl https://sh.rustup.rs -sSf | bash -s -- -y
 export PATH=$HOME_DIRECTORY/.cargo/bin:$PATH
 cargo version
 
 echo "----------------cloning_repository-------------------"
-
 GRAVITY_DIR=$HOME_DIRECTORY/gravity
 ONOMY_DIR=$HOME_DIRECTORY/go/onomy
 git clone -b v0.0.0-20210915184851-orch-nomarket https://github.com/onomyprotocol/cosmos-gravity-bridge.git $GRAVITY_DIR
@@ -42,7 +39,6 @@ cp $GRAVITY_DIR/orchestrator/target/x86_64-unknown-linux-musl/release/gbt $HOME_
 
 echo "---------------Installing_solidity-------------------"
 cd $GRAVITY_DIR/solidity
-dnf -y install nodejs
 npm ci
 chmod -R +x scripts
 npm run typechain
@@ -60,6 +56,3 @@ curl https://get.starport.network/faucet! | bash
 
 export PATH=$PATH:$HOME_DIRECTORY/go/bin
 cd $HOME_DIRECTORY
-
-
-
