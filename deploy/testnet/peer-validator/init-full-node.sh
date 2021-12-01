b2c0b803f31be2b3284040df25cb6379b3f2fee7
Parth4337 <parth.sanghani@yahoo.com>
2021-12-01 01:07:33 -0600

    Updated master/peer node scripts with anom and 10 validators (#56)
    * update master/peer node scripts with anom and 10 validators

--- deploy/testnet/peer-validator/init-full-node.sh
+++ deploy/testnet/peer-validator/init-full-node.sh
@@ -9,7 +9,8 @@ ONOMY_HOME=$HOME/.onomy
 # Name of the network to bootstrap
 #echo "Enter chain-id"
 #read chainid
-CHAINID="onomy-testnet1"
+CHAINID="onomy-testnet
+"
 # Name of the onomy artifact
 ONOMY=onomyd
 
@@ -49,10 +50,10 @@ echo '{
 }' > $ONOMY_HOME/node_info.json
 
 # ------------------ Get IP Address --------------
-ip=$(hostname -I | awk '{print $1}')
+deafult_ip=$(hostname -I | awk '{print $1}')
 
 read -r -p "Enter your ip address [$ip]: " ip
-ip=${ip:-onomy}
+ip=${ip:-deafult_ip}
 
 # ------------------ Init onomy ------------------
 
--- deploy/testnet/peer-validator/start-full-node.sh
+++ deploy/testnet/peer-validator/start-full-node.sh
@@ -3,7 +3,9 @@ set -eu
 
 # Name of the onomy artifact
 ONOMY=onomyd
+# The name of the Onomy node
+ONOMY_NODE_NAME=$(jq -r .node_name $ONOMY_HOME/node_info.json)
 
 echo "Starting onomy full node"
 
-$ONOMY start
+$ONOMY start > $HOME/.onomy/$ONOMY_NODE_NAME.log
