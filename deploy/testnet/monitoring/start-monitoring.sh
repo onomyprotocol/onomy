e74f05864a5c3b04b9413472d9035d86c6ddfb8c
dzmitryhil <dzmitryhil@gmail.com>
2021-12-03 18:04:21 +0300

    * enbale monitoring for single-node-runner * add additional printing to the start-monitoring.sh

--- deploy/testnet/monitoring/start-monitoring.sh
+++ deploy/testnet/monitoring/start-monitoring.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -eu
 
-if [[ -z "${ONOMY-NODE-IP}" ]]; then
+if [[ -z "${ONOMY_NODE_IP}" ]]; then
   echo "The variable ONOMY-NODE-IP is required: "
   exit
 fi
 
+echo "Starting monitoring for: $ONOMY_NODE_IP"
+
 docker-compose up -d
 
-echo "Open the http://localhost:3000/ to access the Grafana, default credentials are: admin:admin:"
+echo "Open the http://localhost:3000/ to access the Grafana, default credentials are: admin:admin"
