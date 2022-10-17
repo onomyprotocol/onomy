1e8f54d78ca37d082980e11c67b68d2cc0f3e980
Dzmitry Hil <dzmitryhil@gmail.com>
2022-10-17 18:41:05 +0300

    Fix undelegate coins error

--- x/dao/keeper/delegation.go
+++ x/dao/keeper/delegation.go
@@ -45,7 +45,7 @@ func (k Keeper) GetDaoDelegationSupply(ctx sdk.Context) sdk.Dec {
 
 // getTargetDelegationState builds a map of the validators and the stake amount they should have now.
 // if the validator is not in the map, the DAO stake is zero.
-func (k Keeper) getTargetDelegationState(ctx sdk.Context, vals []stakingtypes.Validator) map[string]sdk.Dec {
+func (k Keeper) getTargetDelegationState(ctx sdk.Context, vals []stakingtypes.Validator) map[string]sdk.Int {
 	maxValCommission := k.MaxValCommission(ctx)
 	valsSelfBonds := make(map[string]sdk.Dec) // the key is OperatorAddress
 	valsSelfBondsSupply := sdk.ZeroDec()
@@ -73,9 +73,9 @@ func (k Keeper) getTargetDelegationState(ctx sdk.Context, vals []stakingtypes.Va
 
 	daoBondDenomToDelegate := daoBondDenomSupply.Sub(daoBondDenomSupply.Mul(k.PoolRate(ctx)))
 
-	targetDelegationState := make(map[string]sdk.Dec) // the key is OperatorAddress
+	targetDelegationState := make(map[string]sdk.Int) // the key is OperatorAddress
 	for valAddr, selfDelegationAmt := range valsSelfBonds {
-		valDaoDelegationAmt := selfDelegationAmt.Mul(daoBondDenomToDelegate).Quo(valsSelfBondsSupply)
+		valDaoDelegationAmt := selfDelegationAmt.Mul(daoBondDenomToDelegate).Quo(valsSelfBondsSupply).TruncateInt()
 		if !valDaoDelegationAmt.IsZero() {
 			targetDelegationState[valAddr] = valDaoDelegationAmt
 		}
@@ -106,30 +106,30 @@ func (k Keeper) computeDelegationsAndUndelegation(
 	ctx sdk.Context,
 	daoAddr sdk.AccAddress,
 	vals []stakingtypes.Validator,
-	targetDaoStaking map[string]sdk.Dec,
-) (map[string]sdk.Int, map[string]sdk.Dec) {
+	targetDaoStaking map[string]sdk.Int,
+) (map[string]sdk.Int, map[string]sdk.Int) {
 	delegations := make(map[string]sdk.Int)
-	undelegations := make(map[string]sdk.Dec)
+	undelegations := make(map[string]sdk.Int)
 	for _, val := range vals {
 		valAddr := val.GetOperator()
 		targetDaoDelegation, ok := targetDaoStaking[valAddr.String()]
-		delegatedByDao := sdk.ZeroDec()
+		delegatedByDao := sdk.ZeroInt()
 		delegation, found := k.stakingKeeper.GetDelegation(ctx, daoAddr, valAddr)
 		if found {
-			delegatedByDao = val.TokensFromShares(delegation.GetShares())
+			delegatedByDao = val.TokensFromShares(delegation.GetShares()).TruncateInt()
 		}
 		// for the validators not in the target list the target amount is zero
 		if !ok {
-			targetDaoDelegation = sdk.ZeroDec()
+			targetDaoDelegation = sdk.ZeroInt()
 		}
 
-		delegationDelta := targetDaoDelegation.TruncateInt().Sub(delegatedByDao.TruncateInt())
+		delegationDelta := targetDaoDelegation.Sub(delegatedByDao)
 		if delegationDelta.IsZero() {
 			continue
 		}
 
 		if delegationDelta.IsNegative() {
-			undelegations[valAddr.String()] = delegatedByDao.TruncateInt().Sub(targetDaoDelegation.TruncateInt()).ToDec()
+			undelegations[valAddr.String()] = delegatedByDao.Sub(targetDaoDelegation)
 			continue
 		}
 
@@ -139,14 +139,24 @@ func (k Keeper) computeDelegationsAndUndelegation(
 }
 
 // undelegateValidators undelegates the requested amount from the validators in the undelegations.
-func undelegateValidators(ctx sdk.Context, vals []stakingtypes.Validator, undelegations map[string]sdk.Dec, k Keeper, daoAddr sdk.AccAddress) error {
+func undelegateValidators(ctx sdk.Context, vals []stakingtypes.Validator, undelegations map[string]sdk.Int, k Keeper, daoAddr sdk.AccAddress) error {
 	for _, val := range vals {
 		valOper := val.GetOperator()
 		undelegationAmt, ok := undelegations[valOper.String()]
 		if !ok {
 			continue
 		}
-		undelegationShares := val.TokensFromShares(undelegationAmt)
+		if undelegationAmt.IsZero() {
+			continue
+		}
+		undelegationShares, err := val.SharesFromTokens(undelegationAmt)
+		if err != nil {
+			return err
+		}
+		if undelegationShares.IsZero() {
+			continue
+		}
+
 		if _, err := k.stakingKeeper.UnbondAndUndelegateCoins(ctx, daoAddr, valOper, undelegationShares); err != nil {
 			return err
 		}
@@ -163,6 +173,9 @@ func (k Keeper) delegateValidators(ctx sdk.Context, vals []stakingtypes.Validato
 	for _, val := range vals {
 		delegation, ok := delegations[val.GetOperator().String()]
 		if !ok {
+			continue
+		}
+		if delegation.IsZero() {
 			continue
 		}
 		if _, err := k.stakingKeeper.Delegate(ctx, daoAddr, delegation, stakingtypes.Unbonded, val, true); err != nil {
