83df3da1f0993fb7bea11a0fc376d39f9086007f
Aaron Kutch <aaronkutch@att.net>
2023-06-02 16:26:38 -0500

    remove old tests

--- tests/tm-load-test/buid-container.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -eux
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-REPOFOLDER=$DIR/../..
-pushd $REPOFOLDER
-
-docker build -f tests/tm-load-test/Dockerfile  -t cosmoschain .
