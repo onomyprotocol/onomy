34cc09678ddb2b777e9749cbf14076690674a326
Charles Dusek <dusek@onomy.io>
2024-02-22 22:19:51 -0500

    Edit statesync rpc servers

--- deploy/mainnet/chain/scripts/init-statesync.sh
+++ deploy/mainnet/chain/scripts/init-statesync.sh
@@ -11,7 +11,7 @@ ONOMY_HOME_CONFIG="$ONOMY_HOME/config"
 ONOMY_NODE_CONFIG="$ONOMY_HOME_CONFIG/config.toml"
 # App config file for onomy node
 # Statysync servers default IPs
-ONOMY_STATESYNC_SERVERS_DEFAULT_IPS="34.28.250.29,35.224.118.71"
+ONOMY_STATESYNC_SERVERS_DEFAULT_IPS="rpc-mainnet.onomy.io:443,35.224.118.71:26657"
 
 statesync_nodes=
 blk_height=
@@ -21,14 +21,14 @@ blk_hash=
 read -r -p "Enter IPs of statesync nodes (at least 2) [$ONOMY_STATESYNC_SERVERS_DEFAULT_IPS]:" statesync_ips
 statesync_ips=${statesync_ips:-$ONOMY_STATESYNC_SERVERS_DEFAULT_IPS}
 for statesync_ip in ${statesync_ips//,/ } ; do
-  latest_height=$(curl -s http://$statesync_ip:26657/block | jq -r .result.block.header.height);
+  latest_height=$(curl -s http://$statesync_ip/block | jq -r .result.block.header.height);
   trusted_height=$((latest_height - 2000));
 
-  blk_details=$(curl -s http://$statesync_ip:26657/block?height=$trusted_height | jq -r '.result.block.header.height + "\n" + .result.block_id.hash')
+  blk_details=$(curl -s http://$statesync_ip/block?height=$trusted_height | jq -r '.result.block.header.height + "\n" + .result.block_id.hash')
   blk_height=$(echo $blk_details | cut -d$' ' -f1)
 
   blk_hash=$(echo $blk_details | cut -d$' ' -f2)
-  statesync_nodes="$statesync_nodes$statesync_ip:26657,"
+  statesync_nodes="$statesync_nodes$statesync_ip,"
 done
 
 echo "Setting up trusted block number $blk_height and hash $blk_hash"
