44042d812aa0f1236f5c39d4b0a44fa8da3235dc
Charles Dusek <dusek@onomy.io>
2024-03-03 16:20:54 -0600

    Add in environment variables

--- deploy/mainnet/chain/scripts/start-cosmovisor-onomyd.sh
+++ deploy/mainnet/chain/scripts/start-cosmovisor-onomyd.sh
@@ -3,11 +3,14 @@ set -eu
 
 echo "Starting onomy node"
 
-source $HOME/.profile
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/ubuntu/.onomy/bin:/home/ubuntu/.onomy/cosmovisor/genesis/bin
+export DAEMON_HOME=/home/ubuntu/.onomy/
+export DAEMON_NAME=onomyd
+export DAEMON_RESTART_AFTER_UPGRADE=true
 
 if [ "$(ulimit -n)" -lt 65535 ]; then
     echo "Fail ulimit: $(ulimit -n) < 65535"
     exit 1
 fi
 
-cosmovisor start
+$HOME/.onomy/cosmovisor start
