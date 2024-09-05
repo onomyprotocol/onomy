213eb9218345692db8cf81335b05b81360d50fca
sontrinh16 <trinhleson2000@gmail.com>
2024-09-05 11:01:46 +0700

    remove ics

---
+++ app/upgrades/v1.1.6/upgrade.go
@@ -0,0 +1,12 @@
+package v1_1_6 //nolint:revive,stylecheck // app version
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+)
+
+// UpgradeHandler is an x/upgrade handler.
+func UpgradeHandler(_ sdk.Context, _ upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
+	return vm, nil
+}
