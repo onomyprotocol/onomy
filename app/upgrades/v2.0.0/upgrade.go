10840e2ba6572fc39427f96ab3f792526665d04c
Likes To Eat Fish <hoc.sinhphu.lam@gmail.com>
2024-09-30 15:16:05 +0700

    updates upgrade handle and fix  cant unboonding

--- app/upgrades/v2.0.0/upgrade.go
+++ app/upgrades/v2.0.0/upgrade.go
@@ -3,6 +3,8 @@ package v1_1_4 //nolint:revive,stylecheck // app version
 
 import (
 	"context"
+	"fmt"
+	"time"
 
 	"cosmossdk.io/math"
 	upgradetypes "cosmossdk.io/x/upgrade/types"
@@ -28,12 +30,22 @@ func CreateUpgradeHandler(
 	return func(c context.Context, _ upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
 		ctx := sdk.UnwrapSDKContext(c)
 
-		vm, err := mm.RunMigrations(ctx, configurator, vm)
+		ubf, err := getInfoUnbondingFail(ctx, keepers)
+		if err != nil {
+			ctx.Logger().Error("Error fixUnbondingICSRemove:", "message", err.Error())
+		}
+
+		vm, err = mm.RunMigrations(ctx, configurator, vm)
 		if err != nil {
 			return vm, err
 		}
 
-		err = InitializeConsensusParamVersion(ctx, keepers.ConsensusParamsKeeper)
+		err = preMigration2(ctx, keepers, ubf)
+		if err != nil {
+			ctx.Logger().Error("Error fixUnbondingICSRemove:", "message", err.Error())
+		}
+
+		err = initializeConsensusParamVersion(ctx, keepers.ConsensusParamsKeeper)
 		if err != nil {
 			// don't hard fail here, as this is not critical for the upgrade to succeed
 			ctx.Logger().Error("Error initializing ConsensusParam Version:", "message", err.Error())
@@ -41,14 +53,14 @@ func CreateUpgradeHandler(
 
 		err = collectionsInitializeMintParam(ctx, keepers.MintKeeper)
 		if err != nil {
-			// don't hard fail here, as this is not critical for the upgrade to succeed
 			ctx.Logger().Error("Error initializing mint params:", "message", err.Error())
 		}
+
 		return vm, nil
 	}
 }
 
-func InitializeConsensusParamVersion(ctx sdk.Context, consensusKeeper consensusparamkeeper.Keeper) error {
+func initializeConsensusParamVersion(ctx sdk.Context, consensusKeeper consensusparamkeeper.Keeper) error {
 	params, err := consensusKeeper.ParamsStore.Get(ctx)
 	if err != nil {
 		return err
@@ -76,4 +88,97 @@ func collectionsInitializeMintParam(ctx sdk.Context, mintKeeper mintkeeper.Keepe
 	params.InflationRateChange = math.LegacyMustNewDecFromStr("0.130000000000000000")
 	params.MintDenom = "anom"
 	return mintKeeper.Params.Set(ctx, params)
+}
+
+func preMigration2(ctx sdk.Context, keepers *keepers.AppKeepers, ubfs []UnbondingFail) error {
+	for i := 0; i < len(ubfs); i++ {
+		u := ubfs[i]
+		// inbond now
+		ubd, err := keepers.StakingKeeper.GetUnbondingDelegationByUnbondingID(ctx, u.UnbondingId)
+		if err != nil {
+			continue
+		}
+
+		// ddamr bao la dung unbonding entry
+
+		if u.Delegator == ubd.DelegatorAddress && u.Validator == ubd.ValidatorAddress && ubd.Entries[u.Index].Balance.Equal(u.Balance) {
+
+			ubd.Entries[u.Index].UnbondingId = u.UnbondingId
+
+			ubd.Entries[u.Index].CompletionTime = ctx.BlockHeader().Time.Add(-1 * time.Hour)
+			ubd.Entries[u.Index].UnbondingOnHoldRefCount = 1
+
+			err := keepers.StakingKeeper.SetUnbondingDelegation(ctx, ubd)
+			if err != nil {
+				return err
+
+			}
+			err = keepers.StakingKeeper.UnbondingCanComplete(ctx, u.UnbondingId)
+			if err != nil {
+				return err
+			}
+			// index ubd update
+			indexUpdate(u.Delegator, u.Validator, u.Index, ubfs)
+		}
+	}
+
+	return nil
+}
+
+type UnbondingFail struct {
+	Delegator string
+	Validator string
+
+	CreationHeight int64
+	Balance        math.Int
+
+	Index       int
+	UnbondingId uint64
+
+	readed bool
+}
+
+// getInfoUnbondingFail Get info UnbondingFail
+func getInfoUnbondingFail(ctx sdk.Context, keepers *keepers.AppKeepers) (ubf []UnbondingFail, err error) {
+
+	// ubdID errors: 3500-3700 (3599-3678)
+	for i := 3500; i < 3700; i++ {
+		id := uint64(i)
+		ubd, err := keepers.StakingKeeper.GetUnbondingDelegationByUnbondingID(ctx, id)
+		if err != nil {
+			continue // not found ubd for id
+		}
+
+		for j := len(ubd.Entries) - 1; j >= 0; j-- {
+
+			if ubd.Entries[j].UnbondingOnHoldRefCount != 0 && ubd.Entries[j].UnbondingId == id {
+				ubf = append(ubf, UnbondingFail{
+					Delegator:   ubd.DelegatorAddress,
+					Validator:   ubd.ValidatorAddress,
+					Balance:     ubd.Entries[j].Balance,
+					UnbondingId: id,
+					Index:       j,
+					readed:      false,
+				})
+				break
+			}
+		}
+
+	}
+
+	return ubf, nil
+}
+
+func indexUpdate(del, val string, index int, ubf []UnbondingFail) {
+	for i := 0; i < len(ubf); i++ {
+
+		if ubf[i].Delegator == del && ubf[i].Validator == val {
+			if index < ubf[i].Index {
+				fmt.Println("update index:", index)
+				fmt.Println("update index for :", ubf[i].Index)
+
+				ubf[i].Index = ubf[i].Index - 1
+			}
+		}
+	}
 }
