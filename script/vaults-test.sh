1626e4a91fd1e6c3af7675cae22b3d83489037c7
“DongLieu” <duongduydongtq@gmail.com>
2024-10-12 16:46:01 +0700

    new version

--- script/vaults-test.sh
+++ script/vaults-test.sh
@@ -144,13 +144,17 @@ echo "========sleep=========="
 sleep 7
 test1="today auto lazy finger shoulder abstract oppose south sunny glass similar great feature rhythm raise evil owner orange auction absurd half mail ice glory"
 echo $test1 | onomyd keys add test1 --recover --keyring-backend=test --home=$HOME/.onomyd/validator1
-# onomyd keys add test1 --home=$HOME/.onomyd/validator1 --keyring-backend test
 
-onomyd tx bank send $( onomyd keys show validator1 --home=$HOME/.onomyd/validator1  --keyring-backend test -a) $( onomyd keys show test1 --home=$HOME/.onomyd/validator1  --keyring-backend test -a) 30000000nomUSD,10000stake --home=$HOME/.onomyd/validator1 --keyring-backend test --fees 20stake --chain-id testing-1 -y
+test2="convince ocean tower relax toward cattle sort wonder cross enhance pull describe typical total link home glare polar clip trim fish divorce arrest fall"
+echo $test2 | onomyd keys add test2 --recover --keyring-backend=test --home=$HOME/.onomyd/validator1
 
+test3="famous aware lens hair relax cancel glove gloom enforce shoulder spread valley any uncover slush gain dawn slim pipe kidney come exit bench bomb"
+echo $test3 | onomyd keys add test3 --recover --keyring-backend=test --home=$HOME/.onomyd/validator1
+
+onomyd tx bank multi-send $( onomyd keys show validator1 --home=$HOME/.onomyd/validator1  --keyring-backend test -a) $( onomyd keys show test1 --home=$HOME/.onomyd/validator1  --keyring-backend test -a) $( onomyd keys show test2 --home=$HOME/.onomyd/validator1  --keyring-backend test -a) $( onomyd keys show test3 --home=$HOME/.onomyd/validator1  --keyring-backend test -a) 20000000nomUSD,10000stake,10000000atom --home=$HOME/.onomyd/validator1 --keyring-backend test --fees 20stake --chain-id testing-1 -y
 sleep 8
 onomyd q gov proposals
-onomyd tx vaults create-vault 10000000atom 20000000nomUSD --from validator1 --home=$HOME/.onomyd/validator1 --keyring-backend test --fees 20stake --chain-id testing-1 -y
+onomyd tx vaults create-vault 10000000atom 20000000nomUSD --from test2 --home=$HOME/.onomyd/validator1 --keyring-backend test --fees 20stake --chain-id testing-1 -y
 # onomyd tx vaults create-vault 10000000atom 20000000nomUSD --from validator2 --home=$HOME/.onomyd/validator2 --keyring-backend test --fees 20stake --chain-id testing-1 -y
 
 # sleep 7
@@ -171,14 +175,19 @@ onomyd tx vaults create-vault 10000000atom 20000000nomUSD --from validator1 --ho
 
 sleep 7
 
-onomyd q bank balances $(onomyd keys show validator1 -a --keyring-backend test --home /Users/donglieu/.onomyd/validator1)
+onomyd q bank balances $(onomyd keys show test2 -a --keyring-backend test --home /Users/donglieu/.onomyd/validator1)
 
 onomyd tx oracle set-price atom 2 --home=$HOME/.onomyd/validator1  --from validator1 --keyring-backend test --fees 20stake --chain-id testing-1 -y
 
 sleep 31
 onomyd q bank balances $(onomyd keys show test1 --home=$HOME/.onomyd/validator1  --keyring-backend test -a)
 
-onomyd tx auction bid 0 20000000nomUSD 2.2 --from test1 --home=$HOME/.onomyd/validator1 --keyring-backend test --fees 20stake --chain-id testing-1 -y
+onomyd tx auction bid 0 10000000nomUSD 2.2 --from test1 --home=$HOME/.onomyd/validator1 --keyring-backend test --fees 20stake --chain-id testing-1 -y
+
+sleep 7
+
+echo"I will buy all the remaining"
+onomyd tx auction bid 0 20000000nomUSD 2 --from test3 --home=$HOME/.onomyd/validator1 --keyring-backend test --fees 20stake --chain-id testing-1 -y
 
 echo "wating long time, query auction ratecurrent = 1.1...liquidate"
 # onomyd tx aution 
