14d5a26f899214b7904d5b0fca17f4fc761abcfc
vuong177 <nguyenvuong1122000@gmail.com>
2024-10-23 13:56:29 +0700

    migrate params

--- app/upgrades/v2.0.1/upgrade.go
+++ app/upgrades/v2.0.1/upgrade.go
@@ -3,16 +3,18 @@ package v2_0_1 //nolint:revive,stylecheck // app version
 
 import (
 	"context"
+
 	upgradetypes "cosmossdk.io/x/upgrade/types"
+	"github.com/cosmos/cosmos-sdk/baseapp"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
-	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
+	paramstypes "github.com/cosmos/cosmos-sdk/x/params/types"
 
 	"github.com/onomyprotocol/onomy/app/keepers"
 )
 
 // Name is migration name.
-const Name = "v2.0.1"
+const Name = "v2.1.0"
 
 // UpgradeHandler is an x/upgrade handler.
 func CreateUpgradeHandler(
@@ -23,12 +25,14 @@ func CreateUpgradeHandler(
 
 	return func(c context.Context, _ upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
 		ctx := sdk.UnwrapSDKContext(c)
-		keepers.ConsensusParamsKeeper.ParamsStore.Set(ctx, ctx.ConsensusParams())
+		baseAppLegacySS := keepers.ParamsKeeper.Subspace(baseapp.Paramspace).
+			WithKeyTable(paramstypes.ConsensusParamsKeyTable())
+		baseapp.MigrateParams(ctx, baseAppLegacySS, &keepers.ConsensusParamsKeeper.ParamsStore)
 
 		// onomy1vwr8z00ty7mqnk4dtchr9mn9j96nuh6wrlww93
 		addrTreasury := sdk.MustAccAddressFromBech32("onomy1vwr8z00ty7mqnk4dtchr9mn9j96nuh6wrlww93")
 		treasuryTokens := keepers.BankKeeper.GetAllBalances(ctx, addrTreasury)
-		err := keepers.BankKeeper.SendCoinsFromAccountToModule(ctx, addrTreasury, govtypes.ModuleName, treasuryTokens)
+		err := keepers.DistrKeeper.FundCommunityPool(ctx, treasuryTokens, addrTreasury)
 		if err != nil {
 			return vm, err
 		}
