9eb17d61179282396b0f6c82b930d4726c2b6464
hungdinh82 <hungngohihihi@gmail.com>
2024-11-08 15:56:38 +0700

    add final test for JPY

--- script/multi-nom/atom/vaults-test7-jpy.sh
+++ script/multi-nom/atom/vaults-test7-jpy.sh
@@ -6,12 +6,7 @@ sleep 7
 
 onomyd q bank balances $(onomyd keys show test2 -a --keyring-backend test --home $HOME/.onomyd/validator1)
 
-onomyd tx oracle set-price atom 5 --home=$HOME/.onomyd/validator1  --from validator1 --keyring-backend test --fees 20stake --chain-id testing-1 -y
-
-sleep 31
-onomyd q bank balances $(onomyd keys show test1 --home=$HOME/.onomyd/validator1  --keyring-backend test -a)
-
-onomyd tx auction bid 0 9088000000nomJPY 753.3 --from test1 --home=$HOME/.onomyd/validator1 --keyring-backend test --fees 20stake --chain-id testing-1 -y
+onomyd tx oracle set-price atom 4 --home=$HOME/.onomyd/validator1  --from validator1 --keyring-backend test --fees 20stake --chain-id testing-1 -y
 
 echo "wating long time, query auction ratecurrent = 1.1...liquidate"
 # onomyd tx aution 
