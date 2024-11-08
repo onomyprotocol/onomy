f5699590d2ac6acc1d66d8a4930e119e8f412b0d
hungdinh82 <hungngohihihi@gmail.com>
2024-11-08 11:14:09 +0700

    add test JPY for price JPY = 0.0066 USD

--- script/multi-nom/atom/vaults-test1-jpy.sh
+++ script/multi-nom/atom/vaults-test1-jpy.sh
@@ -1,7 +1,7 @@
 sleep 7
 onomyd tx oracle set-price atom 8.0 --home=$HOME/.onomyd/validator3  --from validator3 --keyring-backend test --fees 20stake --chain-id testing-1 -y
 
-onomyd tx vaults create-vault 12500000atom 7550000000nomJPY --from test2 --home=$HOME/.onomyd/validator1 --keyring-backend test --fees 20stake --chain-id testing-1 -y
+onomyd tx vaults create-vault 12500000atom 7575000000nomJPY --from test2 --home=$HOME/.onomyd/validator1 --keyring-backend test --fees 20stake --chain-id testing-1 -y
 sleep 7
 
 onomyd q bank balances $(onomyd keys show test2 -a --keyring-backend test --home $HOME/.onomyd/validator1)
@@ -11,12 +11,12 @@ onomyd tx oracle set-price atom 5 --home=$HOME/.onomyd/validator1  --from valida
 sleep 31
 onomyd q bank balances $(onomyd keys show test1 --home=$HOME/.onomyd/validator1  --keyring-backend test -a)
 
-onomyd tx auction bid 0 2265000000nomJPY 6.0 --from test1 --home=$HOME/.onomyd/validator1 --keyring-backend test --fees 20stake --chain-id testing-1 -y
+onomyd tx auction bid 0 2272000000nomJPY 6.0 --from test1 --home=$HOME/.onomyd/validator1 --keyring-backend test --fees 20stake --chain-id testing-1 -y
 
 sleep 7
 
 echo "I will buy all the remaining"
-onomyd tx auction bid 0 9060000000nomJPY 5.0 --from test3 --home=$HOME/.onomyd/validator1 --keyring-backend test --fees 20stake --chain-id testing-1 -y
+onomyd tx auction bid 0 9088000000nomJPY 5.0 --from test3 --home=$HOME/.onomyd/validator1 --keyring-backend test --fees 20stake --chain-id testing-1 -y
 
 echo "wating long time, query auction ratecurrent = 1.1...liquidate"
-# onomyd tx aution ôn
+# onomyd tx aution 
