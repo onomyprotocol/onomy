ec43ffc39fbcf748d9dd2797ac95e616020342e6
“DongLieu” <duongduydongtq@gmail.com>
2024-11-11 10:33:48 +0700

    ok

--- script/multi-nom/anom/vaults-test1-eur.sh
+++ script/multi-nom/anom/vaults-test1-eur.sh
@@ -1,21 +1,23 @@
-onomyd tx vaults create-vault 12500000atom 50000000nomEUR --from test2 --home=$HOME/.onomyd/validator1 --keyring-backend test --fees 20stake --chain-id testing-1 -y
+sleep 7
+onomyd tx oracle set-price anom 0.03 --home=$HOME/.onomyd/validator3  --from validator3 --keyring-backend test --fees 20stake --chain-id testing-1 -y
+
+onomyd tx vaults create-vault 15000000000anom 50000000nomEUR --from test2 --home=$HOME/.onomyd/validator1 --keyring-backend test --fees 20stake --chain-id testing-1 -y
 
 sleep 7
 
 onomyd q bank balances $(onomyd keys show test2 -a --keyring-backend test --home $HOME/.onomyd/validator1)
 
-onomyd tx oracle set-price atom 5 --home=$HOME/.onomyd/validator1  --from validator1 --keyring-backend test --fees 20stake --chain-id testing-1 -y
+onomyd tx oracle set-price anom 0.02 --home=$HOME/.onomyd/validator1  --from validator1 --keyring-backend test --fees 20stake --chain-id testing-1 -y
 
 sleep 31
 onomyd q bank balances $(onomyd keys show test1 --home=$HOME/.onomyd/validator1  --keyring-backend test -a)
 
-onomyd tx auction bid 0 15000000nomEUR 6.0 --from test1 --home=$HOME/.onomyd/validator1 --keyring-backend test --fees 20stake --chain-id testing-1 -y
+onomyd tx auction bid 0 15000000nomEUR 0.024 --from test1 --home=$HOME/.onomyd/validator1 --keyring-backend test --fees 20stake --chain-id testing-1 -y
 
 sleep 7
 
 echo "I will buy all the remaining"
-onomyd tx auction bid 0 60000000nomEUR 5.0 --from test3 --home=$HOME/.onomyd/validator1 --keyring-backend test --fees 20stake --chain-id testing-1 -y
+onomyd tx auction bid 0 60000000nomEUR 0.02 --from test3 --home=$HOME/.onomyd/validator1 --keyring-backend test --fees 20stake --chain-id testing-1 -y
 
-echo "wating long t
-ime, query auction ratecurrent = 1.1...liquidate"
+echo "wating long time, query auction ratecurrent = 1.1...liquidate"
 # onomyd tx aution 
